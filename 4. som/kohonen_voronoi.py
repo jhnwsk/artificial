8f741115ef0ccf08df74a42a58010d7243131e9c
John <janwasak@gmail.com>
2011-06-01 18:38:27 +0200

    normalise the neurons

--- 4. som/kohonen_voronoi.py
+++ 4. som/kohonen_voronoi.py
@@ -1,15 +1,16 @@
 from self_organising_map import SelfOrganisingMap
-from voronoi import *
+from voronoi import Voronoi
 
 # the 'main' routine 
 if __name__ == "__main__":
    
    # voronoi
    dimensions = [800, 600]
-   input_vectors = [[750, 599], [100, 100], [500, 500], [250, 250], [300, 10]]
+   input_vectors = [[100, 100], [700, 100], [100, 500], [700, 500]]
+
    som = SelfOrganisingMap(dimensions, len(input_vectors))
  
-   for i in range(1):
+   for i in range(2000):
       for iv in input_vectors:
          som.activate(iv)
 
