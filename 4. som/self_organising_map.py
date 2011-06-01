8d4891a64aa5de3e47f50d3dae5678d27e096584
johnDonson <janwasak@gmail.com>
2011-06-01 13:27:32 +0200

    Initial commitment

---
+++ 4. som/self_organising_map.py
@@ -0,0 +1,143 @@
+import random
+import math
+from termcolor import colored
+
+DEBUG = True
+
+def log(value, color = None):
+   """
+   output only if DEBUG
+   """
+   if color == None: color = "cyan"
+   if DEBUG:
+      print colored(value, color)
+
+def output(value, color = None):
+   """
+   output always
+   """
+   if color == None: color = "cyan"
+   print colored(value, color)
+
+#########################################################################################################
+
+class SelfOrganisingMap:
+
+   def __init__(self, input_vector, kohonen_neurons, learning_rate = None):
+      """
+      Initializes SOM with learning_rate
+      """
+      print colored("initializing SOM", "green");
+
+      if learning_rate == None:
+         self.learning_rate = random.uniform(0.01, 0.2)
+      else:
+         self.learning_rate = learning_rate
+     
+      print colored("learning rate {0}", "green").format(self.learning_rate)
+
+      self.kohonen_neurons = []
+
+      self.input_vector = input_vector
+      print colored("input vector {0}", "blue").format(self.input_vector)
+
+      # or if you supplied a tuple with weights
+      if isinstance(kohonen_neurons, (int, long)):
+         for k_n in range(kohonen_neurons):
+            self.kohonen_neurons.append(Neuron(len(input_vector)))
+      # you could've supplied just a number of neurons
+      else:
+         try:
+            for k_n in kohonen_neurons:
+               self.kohonen_neurons.append(Neuron(k_n))
+         except:
+            print colored("invalid kohonen_neurons variable {} supplied!", "red").format(kohonen_neurons)
+
+   def activate(self):
+      """
+      activates SOM
+      """
+      euclidean = {} 
+      for kv in self.kohonen_neurons:
+         euclidean[kv.euclideanDistance(self.input_vector)] = kv
+
+      win = min(euclidean.keys());
+
+      log("and the winner is: {0}".format(win))
+
+      winner = euclidean[win]
+      winner.learn(self.learning_rate, self.input_vector)
+
+#########################################################################################################
+
+class Neuron:
+
+   def __init__(self, dimension, weight = None):
+      """
+      Initializes a new neuron with weight vector
+      """
+      print colored("initializing neuron", "green");
+
+      self.weight = []
+
+      if weight == None:
+         for d in range(dimension):
+            self.weight.append(random.uniform(0, 1))
+      else:
+         self.weight = weight
+
+      print colored("neuron weight vector : {0}", "blue").format(self.weight)
+
+   def euclideanDistance(self, vector):
+      """
+      calculates the euclidian distance between vector and the weight vector of the neuron
+      """
+      if(len(vector) != len(self.weight)):
+         raise SOMError("input {0} and weight {1} vector dimension mismatch".format(len(vector), len(self.weight))) 
+
+      result = 0
+      for i, xi in enumerate(vector):
+         result = result + (xi - self.weight[i])**2
+      
+      log("euclidean before sqrt = {0}".format(result), "cyan")
+
+      result = math.sqrt(result)
+      
+      log("euclidean = {0}".format(result), "cyan") 
+      
+      return result
+
+   def learn(self, learning_rate, vector):
+      """
+      implements the standard competitive learning rule
+      """
+      if(len(vector) != len(self.weight)):
+         raise SOMError("input {0} and weight {1} vector dimension mismatch".format(len(vector), len(self.weight))) 
+      
+      delta_weight = [learning_rate * (xi - self.weight[i]) for i, xi in enumerate(vector)]
+      
+      log("delta weights = {0}".format(delta_weight), "magenta")
+
+      self.weight = [wi + delta_weight[i] for i, wi in enumerate(self.weight)]
+         
+      log("weights after learning = {0}".format(self.weight), "magenta")
+
+
+#########################################################################################################
+
+class SOMError(Exception):
+   def __init__(self, value):
+      self.value = value
+   def __str__(self):
+      return repr(self.value)
+
+# the 'main' routine 
+if __name__ == "__main__":
+   input_neuron = [0.1, 0.1]
+   kohonen_neurons = [[0.5, 0.5], [0.2, 0.3]]
+
+   som = SelfOrganisingMap(input_neuron, 4, 0.1)
+
+   for i in range(100):
+      som.activate()
+
